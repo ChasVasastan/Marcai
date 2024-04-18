9d2513faee3670c8f4b4d025f2e44dec10113aaf
Ecksmas <ludde.oelker@gmail.com>
2024-04-18 13:00:08 +0200

    Added liwpopts.h + connections works to wifi

--- marcai.cpp
+++ marcai.cpp
@@ -1,17 +1,30 @@
+#include <stdio.h>
+
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
+#include "lwipopts.h"
 
-int main() {
+char ssid[] = "id";
+char pass[] = "pass";
+
+int main()
+{
     stdio_init_all();
-    if (cyw43_arch_init()) {
+    if (cyw43_arch_init())
+    {
         printf("Wi-Fi init failed");
         return -1;
     }
-    while (true) {
+
+    cyw43_arch_enable_sta_mode();
+    if (cyw43_arch_wifi_connect_timeout_ms(ssid, pass, CYW43_AUTH_WPA2_AES_PSK, 30000))
+    {
+        printf("failed to connect.\n");
+        return 1;
+    }
+    else
+    {
+        printf("Connected.\n");
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-        sleep_ms(250);
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-        sleep_ms(250);
-        // test
     }
 }
