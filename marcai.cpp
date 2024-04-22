e41177cdb0640f95fdadb81e20563f65f1438c27
Ecksmas <ludde.oelker@gmail.com>
2024-04-22 10:40:28 +0200

    Merged wifi and api branch

--- marcai.cpp
+++ marcai.cpp
@@ -4,27 +4,41 @@
 #include "pico/cyw43_arch.h"
 #include "lwipopts.h"
 
-char ssid[] = "id";
+char ssid[] = "ssid";
 char pass[] = "pass";
+
+bool connect_wifi()
+{
+    cyw43_arch_enable_sta_mode();
+
+    if (cyw43_arch_wifi_connect_timeout_ms(ssid, pass, CYW43_AUTH_WPA2_AES_PSK, 30000))
+    {
+        printf("failed to connect :(\n");
+
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(250);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(250);
+
+        return false;
+    }
+
+    printf("Connected :)\n");
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+    return true;
+}
 
 int main()
 {
-    stdio_init_all();
-    if (cyw43_arch_init())
+    while (!stdio_usb_connected())
     {
-        printf("Wi-Fi init failed");
-        return -1;
     }
 
-    cyw43_arch_enable_sta_mode();
-    if (cyw43_arch_wifi_connect_timeout_ms(ssid, pass, CYW43_AUTH_WPA2_AES_PSK, 30000))
+    stdio_init_all();
+    cyw43_arch_init();
+
+    if (connect_wifi())
     {
-        printf("failed to connect.\n");
-        return 1;
-    }
-    else
-    {
-        printf("Connected.\n");
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        printf("connect wifi error\n");
     }
 }
