b12910cb7da768ce95a6279ee5cc7b1f812d0f17
Olle Mattsson <rymdolle@users.noreply.github.com>
2024-05-02 15:35:42 +0200

    Parse http reply

--- src/http_client.cpp
+++ src/http_client.cpp
@@ -1,5 +1,6 @@
 #include "http_client.h"
 
+#include "lwip/pbuf.h"
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 
@@ -7,9 +8,13 @@
 #include "lwip/altcp.h"
 #include "lwip/dns.h"
 #include "lwip/altcp_tls.h"
+#include <map>
+#include <string>
 
 Http_client::Http_client()
 {
+  http_status = 0;
+  reply_headers_complete = false;
 }
 
 void Http_client::http_get(char *website, char *api_endpoint)
@@ -32,11 +37,82 @@ err_t Http_client::recv(void *arg, struct altcp_pcb *pcb, struct pbuf *p, err_t 
     // TODO: Test this call and try to extract the mp3 file
     // Encode the octect format(binary) or try sending it directly to the encoder
     printf("Recv total %d This buffer %d Next %p Error %d\n", p->tot_len, p->len, p->next, err);
+    uint16_t offset = 0;
+    if (!client->received_status()) {
+      uint16_t end = pbuf_memfind(p, "\r\n", 2, 0);
+      if (end < 0xffff) {
+        std::string status_line(end, 0);
+        pbuf_copy_partial(p, status_line.data(), end, 0);
+        if (auto npos = status_line.find(" "); npos != std::string::npos) {
+          std::string version = status_line.substr(0, npos);
+          int status = std::atoi(status_line.substr(npos).c_str());
+          printf("HTTP Version: %s\n", version.c_str());
+          printf("HTTP Status:  %d\n", status);
+          client->set_status(status);
+          client->set_version(version);
+        } else {
+          panic("Invalid status line\n%s", status_line.c_str());
+        }
+        offset = end + 2;
+      } else {
+        // TODO: handle status not in payload
+        panic("Status outside this buffer");
+      }
+    }
 
-    // Copies data to the buffer
-    pbuf_copy_partial(p, client->myBuff, p->tot_len, 0);
-    client->myBuff[p->tot_len] = 0;
-    printf("Buffer= %s\n", client->myBuff);
+    if (client->received_status() && !client->received_headers()) {
+      uint16_t end = 0xffff;
+      do {
+        end = pbuf_memfind(p, "\r\n", 2, offset);
+        if (end < 0xffff) {
+          int size = end - offset;
+          if (size == 0) {
+            // End of HTTP headers
+            printf("All headers received\n");
+            client->set_received_headers();
+            putchar('\n');
+
+            for (auto [k,v] : client->get_reply_headers()) {
+              // Print processed headers
+              printf("%s: %s\n", k.c_str(), v.c_str());
+            }
+            putchar('\n');
+
+            offset += 2;
+            break;
+          }
+          // Found HTTP header
+          std::string header(size, 0);
+          pbuf_copy_partial(p, header.data(), size, offset);
+
+          // Find header name and value delimiter
+          if (auto npos = header.find(": "); npos != std::string::npos) {
+            std::string key = header.substr(0, npos);
+            std::string value = header.substr(npos + 2);
+            client->add_header(key, value);
+            printf("Raw header: %s: %s\n", key.c_str(), value.c_str());
+          } else {
+            panic("Invalid header\n%s", header.c_str());
+          }
+          offset += size + 2;
+        } else {
+          // TODO: handle headers between pbufs
+          panic("Headers bigger than this buffer");
+        }
+      } while (end < 0xffff);
+
+      if (offset < p->tot_len) {
+        // bytes left in payload
+        printf("remainging payload: %d\n", p->tot_len - offset);
+      }
+    }
+
+    if (client->received_status() && client->received_headers()) {
+      // Copies data to the buffer
+      pbuf_copy_partial(p, client->myBuff, p->tot_len - offset, offset);
+      client->myBuff[p->tot_len - offset] = 0;
+      printf("Buffer= %d bytes\n", p->tot_len - offset);
+    }
 
     // Tells the underlying TCP mechanism that the data has been received
     altcp_recved(pcb, p->tot_len);
