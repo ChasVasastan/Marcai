44d0c9f27b13deb591b2e99896960cb0530fc1c8
Olle Mattsson <rymdolle@users.noreply.github.com>
2024-05-03 12:31:13 +0200

    change path

--- src/marcai.cpp
+++ src/marcai.cpp
@@ -31,25 +31,22 @@ int main()
   }
 
   // Change this to the desired target
-  // char server_name[] = "storage.googleapis.com";
-  // char uri[] = "/udio-artifacts-c33fe3ba-3ffe-471f-92c8-5dfef90b3ea3/samples/8ecb5ec94c774b04bdb3fd66d39c1b2b/1/Cosmic%20Canine%20Cruise.mp3";
-  // char server_name[] = "catfact.ninja";
-  // char uri[] = "/fact";
-
-  char server_name[] = "marcai.blob.core.windows.net";
-  char uri[] = "/audio/mono/YourMom.mp3";
+  char host[] = "marcai.blob.core.windows.net";
+  char path[] = "/audio/mono/YourMom.mp3";
+  // char host[] = "catfact.ninja";
+  // char path[] = "/fact";
 
   Http_client http_client;
-  http_client.http_get(server_name, uri);
+  http_client.http_get(host, path);
 
-  http_client.resolve_dns(server_name);
+  http_client.resolve_dns(host);
   printf("Trying to resolve IP...\n");
   while (!http_client.is_ip_resolved())
   {
     //sleep_ms(500);
   }
 
-  http_client.tls_tcp_setup(server_name);
+  http_client.tls_tcp_setup(host);
 
   while (true)
   {
