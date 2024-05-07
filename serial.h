13ec4ee153aa1f31984725a12671aa0a8a956126
Olle Mattsson <rymdolle@users.noreply.github.com>
2024-05-07 10:13:25 +0200

    Move methods to .cpp

--- serial.h
+++ serial.h
@@ -2,37 +2,16 @@
 #ifndef MARCAI_SERIAL_H
 #define MARCAI_SERIAL_H
 
-#include "hardware/uart.h"
-#include "hardware/gpio.h"
-
-#include <cstdio>
-
 class Serial {
 public:
-  static void init() {
-    uart_init(uart0, 9600);
-    gpio_set_function(0, GPIO_FUNC_UART);
-    gpio_set_function(1, GPIO_FUNC_UART);
-
-    // Set 8 data bits, 1 stop bit, no parity
-    uart_set_format(uart0, 8, 1, UART_PARITY_NONE);
-
-    // Enable irq handler to be called on receive
-    irq_set_exclusive_handler(UART0_IRQ, uart_on_rx);
-    irq_set_enabled(UART0_IRQ, true);
-
-    // Enable irq for rx only
-    uart_set_irq_enables(uart0, true, false);
-  }
+  /** @brief Initialise UART interface
+   */
+  static void init();
 
 private:
-  static void uart_on_rx() {
-    while (uart_is_readable(uart0)) {
-      char c = uart_getc(uart0);
-      putchar(c);
-      uart_putc(uart0, c);
-    }
-  }
+  /** @brief ISR for UART receive
+   */
+  static void uart_on_rx();
 };
 
 #endif /* MARCAI_SERIAL_H */
