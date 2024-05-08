d57eed9d59cc73da0af2f6aa01724b18803ee613
Olle Mattsson <rymdolle@users.noreply.github.com>
2024-05-08 13:06:44 +0200

    Use pbuf instead of vector buffer

--- src/http_client.cpp
+++ src/http_client.cpp
@@ -48,13 +48,18 @@ err_t Http_client::recv(void *arg, struct altcp_pcb *pcb, struct pbuf *p, err_t 
         return ERR_OK;
     }
 
+    if (req->buffer == NULL)
+      req->buffer = p;
+    else
+      pbuf_cat(req->buffer, p);
+
     // TODO: Test this call and try to extract the mp3 file
     // Encode the octect format(binary) or try sending it directly to the encoder
     if (req->status < 100) {
-      uint16_t end = pbuf_memfind(p, "\r\n", 2, 0);
+      uint16_t end = pbuf_memfind(req->buffer, "\r\n", 2, offset);
       if (end < 0xffff) {
         std::string status_line(end, '\0');
-        pbuf_copy_partial(p, status_line.data(), end, 0);
+        pbuf_copy_partial(req->buffer, status_line.data(), end, offset);
         if (auto npos = status_line.find(" "); npos != std::string::npos) {
           std::string version = status_line.substr(0, npos);
           int status = std::atoi(status_line.substr(npos).c_str());
@@ -75,67 +80,38 @@ err_t Http_client::recv(void *arg, struct altcp_pcb *pcb, struct pbuf *p, err_t 
     }
 
     if (req->status >= 100 && !req->received_headers()) {
-      uint16_t end = 0xffff;
+      uint16_t end;
       do {
-        end = pbuf_memfind(p, "\r\n", 2, offset);
+        end = pbuf_memfind(req->buffer, "\r\n", 2, offset);
         if (end < 0xffff) {
-          std::string header;
-          if (req->buffer.size() > 0) {
-            // set correct size if headers overlap pbufs
-            header = std::string(req->buffer.begin(), req->buffer.end());
-            header.resize(req->buffer.size() + end);
-            pbuf_copy_partial(p, header.data() + req->buffer.size(), end, offset);
-            offset = end + 2;
-
-            // We consumed whole buffer so we can clear it
-            req->buffer.clear();
-          } else {
-            header.resize(end - offset, '\0');
-            pbuf_copy_partial(p, header.data(), header.size(), offset);
-            offset = end + 2;
-          }
-
           // Found HTTP header
-          req->add_header(header);
+          std::string header(end - offset, '\0');
+          pbuf_copy_partial(req->buffer, header.data(), header.size(), offset);
+          offset = end + 2;
 
-          if (header.empty()) {
-            // All headers received
-            offset += 2;
-            break;
-          }
-        } else {
-          // Move data to beginnig of buffer if buffer does not
-          // contain all headers
-          req->buffer.resize(p->tot_len - offset);
-          pbuf_copy_partial(p, req->buffer.data(), p->tot_len - offset, offset);
-          printf("Headers bigger buffer, waiting for more... (%ld)\n", req->buffer.size());
+          req->add_header(header);
         }
-      } while (end < 0xffff);
+      } while(end < 0xffff && !req->received_headers());
     }
 
     if (req->status >= 100 && req->received_headers()) {
-      int size = req->buffer.size();
-      req->buffer.resize(size + p->tot_len);
-      pbuf_copy_partial(p, req->buffer.data() + size, p->tot_len - offset, offset);
-
       // Call the callback body function
-      req->body_rx += req->buffer.size();
       if (req->callback_body) {
-        size_t consumed = req->callback_body(req->buffer);
-        req->buffer.erase(req->buffer.begin(), req->buffer.begin() + consumed);
-        printf("Body= %d bytes of %d\n", req->body_rx, req->content_length);
+        std::vector<uint8_t> partial_body(req->buffer->tot_len - offset);
+        pbuf_copy_partial(req->buffer, partial_body.data(), partial_body.size(), offset);
+        offset += req->callback_body(partial_body);
       } else {
-        req->buffer.clear();
+        offset = req->buffer->tot_len;
       }
+
+      req->body_rx += offset;
     }
 
     // Tells the underlying TCP mechanism that the data has been received
-    altcp_recved(pcb, p->tot_len);
+    altcp_recved(pcb, offset);
+    req->buffer = pbuf_free_header(req->buffer, offset);
 
-    // Free the pbuf from memory
-    pbuf_free(p);
-
-    if (req->body_rx == req->content_length) {
+    if (req->body_rx >= req->content_length) {
       printf("\nBody transferred, closing connection\n");
       return altcp_close(pcb);
     }
